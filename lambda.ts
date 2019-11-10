031e594f6d246f1a3ac992983634fe22325ac957
Suherman <suherman@qantas.com.au>
2019-11-10 14:42:41 +1100

    WIP.

--- lambda.ts
+++ lambda.ts
@@ -1,46 +1,76 @@
 const AWS = require('aws-sdk');
-const creds = new AWS.Credentials(
-  process.env.ACCESS_KEY,
-  process.env.SECRET_KEY
-);
+const path = require('path');
+const httpClient = new AWS.HttpClient();
 const zlib = require('zlib');
 
 const host = process.env.ES_ENDPOINT;
 const region = process.env.ES_REGION;
 const endpoint = new AWS.Endpoint(host);
 
+const creds = new AWS.Credentials(
+  process.env.ACCESS_KEY,
+  process.env.SECRET_KEY
+);
+
 const index = 'Content';
 const type = 'Metadata';
+
+// See details from:
+// https://docs.aws.amazon.com/elasticsearch-service/latest/developerguide/search-example.html
+// https://docs.aws.amazon.com/elasticsearch-service/latest/developerguide/es-indexing.html
 
 exports.handler = (event: any, _: any, _callback: any) => {
   console.log('Receive event:', JSON.stringify(event));
 
   event.Records.forEach((item: any) => {
-    /*
-    const cleanUp = function(err) {
+    console.log('item:', item);
+    const keys = unmarshall(item.Keys);
+
+    console.log('keys:', keys);
+
+    const params = {
+      httpMethod: 'PUT',
+      requestPath: 'content-metadata',
+      payload: {
+        // see link above for details
+        settings: {
+          index: {
+            _index: index,
+            _type: type,
+            _id: keys.id,
+          },
+        },
+      },
+    };
+
+    console.log('params:', params);
+    sendRequest(params).then(response => {
+      console.info(response);
+    });
+
+    /*const cleanUp = function(err) {
       if (err instanceof Error === false) {
         err = void 0;
       }
-    };*/
+    };
 
     const request = new AWS.HttpRequest(endpoint);
     request.region = region;
-    request.body = {
+    request.body = JSON.stringify({
       index: {
         _index: index,
         _type: type,
         _id: unmarshall(item.Keys).id,
       },
-    };
+    });
     request.headers['presigned-expires'] = false;
     request.headers['Host'] = host;
 
     // Sign the request (Sigv4)
     const signer = new AWS.Signers.V4(request, 'es');
     signer.addAuthorization(creds, new Date());
-    console.log('signer:', signer);
 
-    /*const send = new AWS.NodeHttpClient();
+    const send = new AWS.NodeHttpClient();
     const req = send.handleRequest(
       request,
       null,
@@ -60,10 +90,9 @@ exports.handler = (event: any, _: any, _callback: any) => {
           response += d;
         });
 
+        console.log('Response:', response);
         incoming.on('error', cleanUp);
         incoming.on('end', cleanUp);
-
-        console.log('Response:', response);
       },
       cleanUp
     );
@@ -74,6 +103,54 @@ exports.handler = (event: any, _: any, _callback: any) => {
     req.setSocketKeepAlive(true);*/
   });
 };
+
+function sendRequest({httpMethod, requestPath, payload}) {
+  const request = new AWS.HttpRequest(endpoint, region);
+
+  request.method = httpMethod;
+  request.headers['Content-Type'] = 'application/json';
+  request.headers['Host'] = host;
+
+  if (requestPath) {
+    request.path = path.join(request.path, requestPath);
+  }
+
+  console.log('payload:', payload);
+  if (payload) {
+    request.body = JSON.stringify(payload);
+  }
+
+  const signer = new AWS.Signers.V4(request, 'es');
+  signer.addAuthorization(creds, new Date());
+
+  return new Promise((resolve, reject) => {
+    httpClient.handleRequest(
+      request,
+      null,
+      response => {
+        const {statusCode, statusMessage, headers} = response;
+        let body = '';
+        response.on('data', chunk => {
+          body += chunk;
+        });
+        response.on('end', () => {
+          const data: any = {
+            statusCode,
+            statusMessage,
+            headers,
+          };
+          if (body) {
+            data.body = JSON.parse(body);
+          }
+          resolve(data);
+        });
+      },
+      err => {
+        reject(err);
+      }
+    );
+  });
+}
 
 function unmarshall(data: any) {
   return AWS.DynamoDB.Converter.unmarshall(data);
